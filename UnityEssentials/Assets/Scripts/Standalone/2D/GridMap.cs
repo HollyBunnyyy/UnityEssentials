3a23fc6ba9a51b2b2167e35e4d956adf95c93e36
HollyBunnyyy <HollyMadisonEstelle@gmail.com>
2023-08-12 01:57:39 -0700

    hotfix for the GridMap class - oops!
    Was originally using CellSize instead of CellArea in some of the methods for calculation - pretty big oversight but should be fixed.

--- UnityEssentials/Assets/Scripts/Standalone/2D/GridMap.cs
+++ UnityEssentials/Assets/Scripts/Standalone/2D/GridMap.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is basically the native unity grid type, but open source so you can see all the inner workings.
 public class GridMap<T>
 {
     public readonly int Width;
@@ -22,12 +23,14 @@ public class GridMap<T>
 
     }
 
-    public GridMap( int width, int height )
+    public GridMap( int width, int height, Vector2 cellSize, Vector2 cellGap )
     {
-        this.Width  = width;
-        this.Height = height;
+        this.Width      = width;
+        this.Height     = height;
+        this.CellGap    = cellSize;
+        this.CellSize   = cellGap;
 
-        this._tileGrid = new T[width, height];
+        this._tileGrid  = new T[width, height];
 
         for( int x = 0; x < width; x++ )
         {
@@ -37,14 +40,6 @@ public class GridMap<T>
 
             }
         }
-
-    }
-
-    public GridMap( int width, int height, Vector2 cellSize, Vector2 cellGap ) : this( width, height )
-    {
-        this.CellSize = cellSize;
-        this.CellGap  = cellGap;
-
     }
 
     /// <summary>
@@ -63,8 +58,8 @@ public class GridMap<T>
     {
         Vector2Int cellPositionOnGrid = new Vector2Int()
         {
-            x = Mathf.RoundToInt( ( worldPosition.x + Origin.x ) / CellSize.x ),
-            y = Mathf.RoundToInt( ( worldPosition.y + Origin.y ) / CellSize.y )
+            x = Mathf.RoundToInt( ( worldPosition.x + Origin.x ) / CellArea.x ),
+            y = Mathf.RoundToInt( ( worldPosition.y + Origin.y ) / CellArea.y )
 
         };
 
@@ -99,8 +94,8 @@ public class GridMap<T>
 
         foreach( Direction2D direction in directionType )
         {
-            int xIndex = x + Mathf.FloorToInt( direction.X );
-            int yIndex = y + Mathf.FloorToInt( direction.Y );
+            int xIndex = x + direction.X;
+            int yIndex = y + direction.Y;
 
             if( IsIndexInMap( xIndex, yIndex ) )
             {
@@ -125,7 +120,7 @@ public class GridMap<T>
     }
 
     /// <summary>
-    /// Returns a dictionary with corresponding tile objects mapped to their directions relative to the index of x and y.
+    /// Returns a dictionary with corresponding tile objects mapped with their directions relative to the index of x and y.
     /// </summary>
     public Dictionary<Direction2D, T> GetSurroundingTileDirections( int x, int y, DirectionType directionType )
     {
@@ -133,12 +128,12 @@ public class GridMap<T>
 
         foreach( Direction2D direction in directionType )
         {
-            int xIndex = x + Mathf.FloorToInt( direction.X );
-            int yIndex = y + Mathf.FloorToInt( direction.Y );
+            int xIndex = x + direction.X;
+            int yIndex = y + direction.Y;
 
             if( IsIndexInMap( xIndex, yIndex ) )
             {
-                surroundingNodes.Add( direction, _tileGrid[xIndex, yIndex] );
+                surroundingNodes.Add( direction, this[xIndex, yIndex] );
 
             }
         }
@@ -148,7 +143,7 @@ public class GridMap<T>
     }
 
     /// <summary>
-    /// Returns a kvp dictionary with corresponding objects mapped to their directions relative to the given worldposition.
+    /// Returns a dictionary with corresponding tile objects mapped with their directions relative to the given world position.
     /// </summary>
     public Dictionary<Direction2D, T> GetSurroundingTileDirections( Vector3 worldPosition, DirectionType directionType )
     {
@@ -167,8 +162,8 @@ public class GridMap<T>
         Vector2 clampedCellPosition = WorldToTilePosition( worldPositionConvertedToGrid - ( Origin + CellCenter ) );
 
         Vector3 clampedWorldPosition = clampedCellPosition;
-        clampedWorldPosition.x *= CellSize.x;
-        clampedWorldPosition.y *= CellSize.y;
+        clampedWorldPosition.x *= CellArea.x;
+        clampedWorldPosition.y *= CellArea.y;
         clampedWorldPosition.z = worldPosition.z;
 
         clampedWorldPosition.x += CellCenter.x;
